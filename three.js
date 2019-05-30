6db5093db8de0a0b42c799ee8d7c0b68ca550d6b
srafi1 <shakilrafi0@gmail.com>
2019-05-30 13:37:44 -0400

    fixed refresh bug on three.html

--- three.js
+++ three.js
@@ -2,6 +2,8 @@ function check() {
     let passw = document.getElementsByName("passw");
     if (passw[0].value == "12345678") {
         document.getElementById("msg").innerHTML = "You shall pass";
+    } else {
+        alert("Incorrect password");
     }
     return false;
 }
